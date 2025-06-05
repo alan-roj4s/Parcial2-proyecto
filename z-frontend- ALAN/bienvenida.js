c790db97294353187f7e6822f1974ee733f3e4fb
lefish <alanrojascuentaescolar@gmail.com>
2025-06-04 21:14:41 -0300

    test 1
     test 2

--- z-frontend- ALAN/bienvenida.js
+++ z-frontend- ALAN/bienvenida.js
@@ -1,8 +1,52 @@
 const input = document.getElementById("nombre");
-const mensaje = document.getElementById("mensaje");
+const form = document.getElementById("ingreso")
+const test = document.getElementById("test")
 
-input.addEventListener("input", () => {
-    if (input.value.toLowerCase() === "anana") {
-        mensaje.textContent = "Se escribió anana";
+
+let usuarios = []; //este es solo simulacion
+
+
+
+function ingreso(event) {
+    event.preventDefault(); 
+
+    if (input.value.toLowerCase() != "") {
+
+        // aqui se debe insertar un llamado al backend, en el que
+        // llamara al servidor para que el server 
+        // inserte el nombre y la id en el json
+
+
+        // a continuacion, una simulacion
+        const insertarUsuario = {
+            nombre: input.value.trim()
+        }
+        usuarios.push(insertarUsuario)
+
+
+
+        // este solo muestra el nombre mas reciente
+        test.textContent = `ultimo nombre: ${insertarUsuario.nombre}`;
+
+
+
+
+        // window.location.href = "placeholder.html"; // Redirige
+    } 
+    else {
+        alert ("por favor ingrese un nombre")
     }
-});
+}
+
+// asocia el form al evento submit
+form.addEventListener("submit", ingreso);
+
+
+
+
+
+
+
+// HECHO --------------------------agregar un boton en el form de continuar, al apretarlo este debe llevar al placeholder
+// una vez que se compruebe que funciona, crear un json en el que
+// se guarde la informacion del usuario en el caso que no exista
